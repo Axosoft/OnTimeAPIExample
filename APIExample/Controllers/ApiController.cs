47abc3137453bb4966d69586b8775aa4ad5ff452
Stjepan Rajko <stjepanr@axosoft.com>
2012-09-04 09:02:06 -0700

    added bootstrap to Api call page, displaying api response in-page
    [otf:9249]

--- APIExample/Controllers/ApiController.cs
+++ APIExample/Controllers/ApiController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using OnTimeApi;
+using System.Net;
 
 namespace APIExample.Controllers
 {
@@ -22,7 +23,16 @@ namespace APIExample.Controllers
 		{
 			// make an API call to OnTime
 			var OnTime = new OnTime(MvcApplication.Settings, (string)Session["AccessToken"]);
-			return Redirect(OnTime.GetUrl("projects"));
+
+			var webClient = new WebClient();
+
+			var resultString = webClient.DownloadString(OnTime.GetUrl("projects"));
+			Response.Write(resultString);
+			Response.ContentType = "application/json";
+			Response.ContentEncoding = System.Text.Encoding.UTF8;
+			Response.Charset = "UTF-8";
+
+			return null;
 		}
 
     }
