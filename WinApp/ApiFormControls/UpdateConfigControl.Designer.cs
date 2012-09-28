fe8df991ccdc5b2cf886f45afef7b4870b821e36
Stjepan Rajko <stjepanr@axosoft.com>
2012-09-28 16:34:34 -0700

    adding links to developer.ontimenow.com/getting-started
    [otf:9249]

--- WinApp/ApiFormControls/UpdateConfigControl.Designer.cs
+++ WinApp/ApiFormControls/UpdateConfigControl.Designer.cs
@@ -30,6 +30,7 @@
 		{
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -53,11 +54,22 @@
 			this.label2.Text = "Please close the application window and update your App.config file with your OnT" +
     "ime URL, Client ID and Client Secret.";
 			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(5, 58);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(560, 13);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "You can find more information on obtaining a Client ID and Secret at http://devel" +
+    "oper.ontimenow.com/getting-started.";
+			// 
 			// UpdateConfigControl
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.White;
+			this.Controls.Add(this.label3);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Name = "UpdateConfigControl";
@@ -71,5 +83,6 @@
 
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
 	}
 }
